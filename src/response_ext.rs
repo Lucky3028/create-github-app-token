a667201d00e0e4c8d971089051a0954fcf823304
Lucky3028 <lucky3028@users.noreply.github.com>
2021-12-12 13:23:09 +0900

    [Fix] format

--- src/response_ext.rs
+++ src/response_ext.rs
@@ -1,17 +1,18 @@
+use crate::errors::{new_error, Error, ErrorKind, Result};
 use surf::StatusCode;
 
 type SurfResult = std::result::Result<surf::Response, surf::Error>;
 
 pub(crate) trait ResponseExt {
-    fn convert(self) -> crate::errors::Result<surf::Response>;
+    fn convert(self) -> Result<surf::Response>;
 }
 
 impl ResponseExt for SurfResult {
-    fn convert(self) -> crate::errors::Result<surf::Response> {
+    fn convert(self) -> Result<surf::Response> {
         let res = match self {
             Ok(res) => res,
             Err(e) => {
-                return Err(crate::errors::Error::from(e));
+                return Err(Error::from(e));
             }
         };
         let status = res.status();
@@ -20,13 +21,9 @@ impl ResponseExt for SurfResult {
         }
 
         Err(match status {
-            StatusCode::NotFound => {
-                crate::errors::new_error(crate::errors::ErrorKind::ResourceNotFound)
-            }
-            StatusCode::Unauthorized => {
-                crate::errors::new_error(crate::errors::ErrorKind::UnAuthorized)
-            }
-            _ => crate::errors::new_error(crate::errors::ErrorKind::UnknownStatusCode(status)),
+            StatusCode::NotFound => new_error(ErrorKind::ResourceNotFound),
+            StatusCode::Unauthorized => new_error(ErrorKind::UnAuthorized),
+            _ => new_error(ErrorKind::UnknownStatusCode(status)),
         })
     }
 }
